2923955742509c330f6857b438ffe7dea1b99c11
JongminChoi <joeychoi7624@gmail.com>
2023-01-31 13:10:03 -0800

    [Update] useState to reactQuery

---
+++ src/api.ts
@@ -0,0 +1,10 @@
+const BASE_URL = `https://api.coinpaprika.com/v1`;
+
+export const fetchCoins = () =>
+  fetch(`${BASE_URL}/coins`).then((response) => response.json());
+
+export const fetchCoinInfo = (coinId: string) =>
+  fetch(`${BASE_URL}/coins/${coinId}`).then((response) => response.json());
+
+export const fetchCoinTickers = (coinId: string) =>
+  fetch(`${BASE_URL}/tickers/${coinId}`).then((response) => response.json());
